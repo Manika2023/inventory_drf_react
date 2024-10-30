90aecc2ead2ba256f7f1ede12d3c8db888f2f8a2
manika <singhmanika409@gmail.com>
2024-10-30 12:31:49 +0530

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,13 +14,11 @@ export default function Footer() {
     <footer className="bg-gray-100 text-gray-900 pt-8 mt-auto">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-4">
         <div className="mb-4 md:mb-0">
-          <h2 className="text-2xl font-bold text-black">CRUD</h2>
+          <h2 className="text-2xl font-bold text-black">Inventory Management System</h2>
           <p className="text-gray-900 mt-2">Sharing knowledge, one solution at a time.</p>
         </div>
         <div className="flex flex-col md:flex-row md:space-x-6">
           <NavLink to="/" className={`hover:text-gray-700 ${isActive('/')}`}>Home</NavLink>
-          <NavLink to="/about" className={`hover:text-gray-700 ${isActive('/about')}`}>About</NavLink>
-          <NavLink to="/blog" className={`hover:text-red-500 ${isActive('/blog')}`}>Blog</NavLink>
           <NavLink to="/register" className={`hover:text-red-500 ${isActive('/register')}`}>Sign Up</NavLink>
           <NavLink to="/login" className={`hover:text-red-500 ${isActive('/login')}`}>Login</NavLink>
         </div>
@@ -40,7 +38,7 @@ export default function Footer() {
         </div>
       </div>
       <div className="bg-gray-300 py-4 mt-3 text-center text-gray-900  bottom-0 w-full">
-        <p>&copy; {new Date().getFullYear()} MyBlog. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} Inventory Management System. All rights reserved.</p>
       </div>
     </footer>
   );
